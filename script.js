f9ffe279847c5500dcced833fea7878f5aca4183
Rajatluharuka <rajatluharuka123>
2023-09-27 19:49:28 +0530

    Task 1 updated

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ document.getElementById("registration-form").addEventListener("submit", function
     event.preventDefault();
 
     const formData = new FormData(event.target);
+    const data = {};
 
     formData.forEach((value, key) => {
-        localStorage.setItem(key,value);
+        data[key] = value;
     });
+    const userData = JSON.stringify(data);
+    localStorage.setItem("user",userData);
 });
