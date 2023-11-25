7f72743c21032f82e5d075a103ecf22aa9176c89
Rajatluharuka <rajatluharuka123>
2023-11-25 20:50:42 +0530

    Task 8 updated

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+let naam=document.getElementById("name");
+let email=document.getElementById("email");
+let phone=document.getElementById("phone");
+let date=document.getElementById("start");
+let time=document.getElementById("time");
 let forms=document.getElementById("registration-form");
 let userList=document.createElement('ul');
 userList.className = 'userList';
 userList.id = 'list';
 forms.appendChild(userList);
+let remUser = 0;
 
 document.getElementById("registration-form").addEventListener("submit", function(event) {
     event.preventDefault();
@@ -13,16 +19,25 @@ document.getElementById("registration-form").addEventListener("submit", function
     formData.forEach((value, key) => {
         data[key] = value;
     });
-    axios.post("https://crudcrud.com/api/c727bc2b952e43d5aaa5c30edeb5f594/appointmentData",data)
+    if(remUser === 0){
+    axios.post("https://crudcrud.com/api/de76fa8d5be74f349172845eef90623c/appointmentData",data)
     .then((response) => {
         showData(response.data);
     }).catch((err) => {
         console.log(err);
     });
+}else{
+    axios.put(`https://crudcrud.com/api/de76fa8d5be74f349172845eef90623c/appointmentData/${remUser}`,data)
+    .then((response) => {
+        showData(data);
+    }).catch((err) => {
+        console.log(err);
+    });
+}
 });
 
 window.addEventListener("DOMContentLoaded",()=>{
-    axios.get("https://crudcrud.com/api/c727bc2b952e43d5aaa5c30edeb5f594/appointmentData")
+    axios.get("https://crudcrud.com/api/de76fa8d5be74f349172845eef90623c/appointmentData")
     .then((response) => {
         for(let i=0;i<response.data.length;i++){
         showData(response.data[i]);
@@ -38,7 +53,7 @@ document.getElementById("list").addEventListener('click',function(e) {
         let remUser = e.target.parentElement.dataset.id;
         document.getElementById("list").removeChild(user);
 
-    axios.delete(`https://crudcrud.com/api/c727bc2b952e43d5aaa5c30edeb5f594/appointmentData/${remUser}`)
+    axios.delete(`https://crudcrud.com/api/de76fa8d5be74f349172845eef90623c/appointmentData/${remUser}`)
     .then((response) => {
         console.log(response);
     }).catch((err) => {
@@ -47,6 +62,20 @@ document.getElementById("list").addEventListener('click',function(e) {
  }
 });
 
+document.getElementById("list").addEventListener('click',function(e) {
+    if(e.target.classList.contains('edit')){
+        let user=e.target.parentElement;
+        remUser = e.target.parentElement.dataset.id;
+
+        naam.value = user.childNodes[0].textContent;
+        email.value = user.childNodes[2].textContent;
+        phone.value = user.childNodes[4].textContent; 
+        date.value = user.childNodes[6].textContent;
+        time.value = user.childNodes[8].textContent; 
+        
+        document.getElementById("list").removeChild(user);
+    }
+});
 function showData(userData)
 {
     let user=document.createElement('li');
